a12f322f170ac4e4c1286462028bdf518558d082
hosoda riku <r.hosokada.sys20@morijyobi.ac.jp>
2021-03-30 12:57:01 +0900

    コミット

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request
 import calc
+import kadaiA
 
 app = Flask(__name__)
 
@@ -16,6 +17,18 @@ def calc_circle():
     r = int(request.args.get("r"))
     result = calc.calc_circle(r)
     return render_template("result_value.html",result=result)
+
+@app.route("/keisan")
+def keisan():
+    return render_template("kadai_keisan.html")
+
+@app.route("/keisan_result")
+def keisan_result():
+    kyuu1 = int(request.args.get("a"))
+    kyuu2 = int(request.args.get("b"))
+    result = kyuu1 * kyuu2
+    #result = kadaiA.keisan_result(kyuu)
+    return render_template("keisan_result.html",result=result)
 
 
 if __name__ == "__main__":
---
+++ kadaiA.py
@@ -0,0 +1,3 @@
+def keisan_result(a, b):
+    kyuu = a * b
+    return kyuu
