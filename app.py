8f399ccf066f64e64ecc4421ed4b4c2a3ea49291
hira-Git <d.hirasawa.sys20@morijyobi.ac.jp>
2021-03-30 12:14:08 +0900

    円の計算機能の作成

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import calc
 
 app = Flask(__name__)
 
 @app.route("/")
 def top():
     return render_template("index.html")
+
+@app.route("/circle")
+def circle():
+    return render_template("circle.html")
+
+@app.route("/calc_circle")
+def calc_circle():
+    r = int(request.args.get("r"))
+    result = calc.calc_circle(r)
+    return render_template("result_value.html",result=result)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
